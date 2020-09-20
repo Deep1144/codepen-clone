627ccf1927c9b0608c3dae8f103b0a27a89ca296
Deep <pateldeep5724@gmail.com>
2020-09-20 16:57:58 +0530

    Updates

---
+++ precache-manifest.baaab358b243c253178c1935e194bfe3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72863607d2b302a77207eb0d2c9c9e32",
+    "url": "/codepen-clone/index.html"
+  },
+  {
+    "revision": "ce940ff3ddbfcd45f782",
+    "url": "/codepen-clone/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "a38b1388a127ab8c718c",
+    "url": "/codepen-clone/static/css/main.3b5b6ea7.chunk.css"
+  },
+  {
+    "revision": "ce940ff3ddbfcd45f782",
+    "url": "/codepen-clone/static/js/2.d58eb044.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codepen-clone/static/js/2.d58eb044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a38b1388a127ab8c718c",
+    "url": "/codepen-clone/static/js/main.2d00c345.chunk.js"
+  },
+  {
+    "revision": "7e95b33d3c3f8b32c9c4",
+    "url": "/codepen-clone/static/js/runtime-main.3a6f25c4.js"
+  }
+]);
